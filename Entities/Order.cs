9956b9a65ccd349c24f82efd2dd3699a1e2d7c31
Ícaro Monteiro <icarosilvamonteiro@gmail.com>
2022-09-04 12:40:06 -0300

    Criado o "ToString".

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -10,7 +10,7 @@ namespace Csharp_EnumEComposicao.Entities
     internal class Order
     {
         public DateTime Moment { get; set; }
-        public Enum OrderStatus { get; set; }
+        public OrderStatus Status { get; set; }
         public Client Client { get; set; }
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 
@@ -45,6 +45,22 @@ namespace Csharp_EnumEComposicao.Entities
             }
 
             return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items: ");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(Items.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
         }
     }
 }
