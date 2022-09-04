f22897bd6ce35532d26e82b7ecaae0f590537aa4
Ícaro Monteiro <icarosilvamonteiro@gmail.com>
2022-09-04 12:11:12 -0300

    Criadas as estruturas iniciais/classes do projeto

---
+++ Entities/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Csharp_EnumEComposicao.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            this.email = email;
+            BirthDate = birthDate;
+        }
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Csharp_EnumEComposicao.Entities.Enums;
+
+
+namespace Csharp_EnumEComposicao.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public Enum OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, Enum status, Client client)
+        {
+            Moment = moment;
+            OrderStatus = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum;
+        }
+    }
+}
